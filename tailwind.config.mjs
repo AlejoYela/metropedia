76bdcda518f7aa94797c87c93b47ceb23cfa0017
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-06-20 16:12:51 -0500

    Add: About section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'selector',
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary:"#fcb316"
+			}
+		},
 	},
 	plugins: [],
 }
