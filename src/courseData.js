f178c3a3e866119f9d40aaa04daa45495cd5ef65
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-11-05 20:06:15 -0500

    Dashboard functionality

---
+++ src/courseData.js
@@ -0,0 +1,37 @@
+// courseData.js
+
+const courseData = [
+    {
+      course: "METROLOGÍA BÁSICA",
+      image: "/path/to/metrologia-basica.png",  // Cambia a la ruta real de la imagen
+      color: "#60a5fa",  // Un color representativo para este curso
+    },
+    {
+      course: "VERIFICACIÓN DE MÉTODOS",
+      image: "/path/to/verificacion-metodos.png",
+      color: "#f59e0b",
+    },
+    {
+      course: "REGLAS DE DECISIÓN",
+      image: "/path/to/reglas-decision.png",
+      color: "#10b981",
+    },
+    {
+      course: "ISO/IEC 10012:2003 SISTEMAS DE GESTIÓN DE LA MEDICIÓN",
+      image: "/path/to/iso-10012.png",
+      color: "#d946ef",
+    },
+    {
+      course: "VOCABULARIO INTERNACIONAL DE METROLOGÍA",
+      image: "/path/to/vocabulario-mt.png",
+      color: "#f43f5e",
+    },
+    {
+      course: "ESTIMACIÓN DE LA INCERTIDUMBRE",
+      image: "/path/to/estimacion-incertidumbre.png",
+      color: "#3b82f6",
+    }
+  ];
+  
+  export default courseData;
+  
