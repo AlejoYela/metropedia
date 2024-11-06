5851d8a8db4aac5cc749cdbe03a7ae9feed59dc6
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-11-06 13:39:50 -0500

    Embed certificate validation page

--- src/courseData.js
+++ src/courseData.js
@@ -3,35 +3,37 @@
 const courseData = [
     {
       course: "METROLOGÍA BÁSICA",
-      image: "/courses/1.png",  // Cambia a la ruta real de la imagen
+      image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907986/1_m54p9f.png",  // Cambia a la ruta real de la imagen
       color: "#60a5fa",  // Un color representativo para este curso
     },
     {
       course: "VERIFICACIÓN DE MÉTODOS",
-      image: "/courses/2.png",  // Cambia a la ruta real de la imagen
+      image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907986/2_h9fb5f.png",  // Cambia a la ruta real de la imagen
       color: "#ff7900",
     },
     {
       course: "REGLAS DE DECISIÓN",
-      image: "/courses/3.png",  // Cambia a la ruta real de la imagen
+      image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/3_cbbkx2.png",  // Cambia a la ruta real de la imagen
       color: "#ac94fa",
     },
     {
       course: "ISO/IEC 10012:2003 SISTEMAS DE GESTIÓN DE LA MEDICIÓN",
-      image: "/courses/4.png",  // Cambia a la ruta real de la imagen
+      image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/4_deyo3e.png",  // Cambia a la ruta real de la imagen
       color: "#16bdca",
     },
     {
       course: "VOCABULARIO INTERNACIONAL DE METROLOGÍA",
-      image: "/courses/5.png",  // Cambia a la ruta real de la imagen
+      image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/5_rvkyiw.png",  // Cambia a la ruta real de la imagen
       color: "#f43f5e",
     },
     {
       course: "ESTIMACIÓN DE LA INCERTIDUMBRE",
-      image: "/courses/6.png",  // Cambia a la ruta real de la imagen
+      image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/6_ojgioi.png",  // Cambia a la ruta real de la imagen
       color: "#3b82f6",
     }
   ];
+
+  
   
   export default courseData;
   
