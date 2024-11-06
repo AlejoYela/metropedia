dd33ca2610dc4db27d4f849d0ee5bbf1b410e686
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-11-06 00:10:08 -0500

    Better campus dashboard styles

--- src/courseData.js
+++ src/courseData.js
@@ -3,32 +3,32 @@
 const courseData = [
     {
       course: "METROLOGÍA BÁSICA",
-      image: "/path/to/metrologia-basica.png",  // Cambia a la ruta real de la imagen
+      image: "/courses/1.png",  // Cambia a la ruta real de la imagen
       color: "#60a5fa",  // Un color representativo para este curso
     },
     {
       course: "VERIFICACIÓN DE MÉTODOS",
-      image: "/path/to/verificacion-metodos.png",
-      color: "#f59e0b",
+      image: "/courses/2.png",  // Cambia a la ruta real de la imagen
+      color: "#ff7900",
     },
     {
       course: "REGLAS DE DECISIÓN",
-      image: "/path/to/reglas-decision.png",
-      color: "#10b981",
+      image: "/courses/3.png",  // Cambia a la ruta real de la imagen
+      color: "#ac94fa",
     },
     {
       course: "ISO/IEC 10012:2003 SISTEMAS DE GESTIÓN DE LA MEDICIÓN",
-      image: "/path/to/iso-10012.png",
-      color: "#d946ef",
+      image: "/courses/4.png",  // Cambia a la ruta real de la imagen
+      color: "#16bdca",
     },
     {
       course: "VOCABULARIO INTERNACIONAL DE METROLOGÍA",
-      image: "/path/to/vocabulario-mt.png",
+      image: "/courses/5.png",  // Cambia a la ruta real de la imagen
       color: "#f43f5e",
     },
     {
       course: "ESTIMACIÓN DE LA INCERTIDUMBRE",
-      image: "/path/to/estimacion-incertidumbre.png",
+      image: "/courses/6.png",  // Cambia a la ruta real de la imagen
       color: "#3b82f6",
     }
   ];
