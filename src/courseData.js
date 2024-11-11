c25922af3b9c95087bfdb669dd1a47e774cbf06b
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-11-11 00:56:27 -0500

    Individual course page

--- src/courseData.js
+++ src/courseData.js
@@ -2,49 +2,94 @@
 
 const courseData = [
     {
+      id: 1,
       course: "METROLOGÍA BÁSICA",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907986/1_m54p9f.png",  // Cambia a la ruta real de la imagen
-      color: "#60a5fa",  // Un color representativo para este curso
+      color: "#60a5fa",
+      description: "",
+      modules: ["","",""],
+      category: "Metrología",
+      price: 30000
     },
     {
+      id: 2,
       course: "VERIFICACIÓN DE MÉTODOS",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907986/2_h9fb5f.png",  // Cambia a la ruta real de la imagen
       color: "#ff7900",
+      description: "",
+      modules: ["","",""],
+      category: "Metrología",
+      price: 30000
     },
     {
+      id: 3,
       course: "REGLAS DE DECISIÓN",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/3_cbbkx2.png",  // Cambia a la ruta real de la imagen
       color: "#ac94fa",
+      description: "",
+      modules: ["","",""],
+      category: "Metrología",
+      price: 30000
     },
     {
+      id: 4,
       course: "ISO/IEC 10012:2003 SISTEMAS DE GESTIÓN DE LA MEDICIÓN",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/4_deyo3e.png",  // Cambia a la ruta real de la imagen
       color: "#16bdca",
+      description: "",
+      modules: ["","",""],
+      category: "Normativa internacional",
+      price: 30000
     },
     {
+      id: 5,
       course: "VOCABULARIO INTERNACIONAL DE METROLOGÍA",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/5_rvkyiw.png",  // Cambia a la ruta real de la imagen
       color: "#f43f5e",
+      description: "",
+      modules: ["","",""],
+      category: "Metrología",
+      price: 30000
     },
     {
+      id: 6,
       course: "ESTIMACIÓN DE LA INCERTIDUMBRE",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1730907987/6_ojgioi.png",  // Cambia a la ruta real de la imagen
       color: "#3b82f6",
+      description: "",
+      modules: ["","",""],
+      category: "Metrología",
+      price: 30000
     },
     {
+      id: 7,
       course: "ISO/IEC 19011:2018 AUDITORÍA DE SISTEMAS DE GESTIÓN",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1731014114/7_nur6p4.png",  // Cambia a la ruta real de la imagen
       color: "#60a5fa",
+      description: "",
+      modules: ["","",""],
+      category: "Normativa internacional",
+      price: 30000
     },
     {
+      id: 8,
       course: "ISO/IEC 31000:2018 GESTIÓN DEL RIESGO",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1731014114/8_hniuy2.png",  // Cambia a la ruta real de la imagen
       color: "#60a5fa",
+      description: "",
+      modules: ["","",""],
+      category: "Normativa internacional",
+      price: 30000
     },
     {
+      id: 9,
       course: "INDICADORES DE GESTIÓN",
       image: "https://res.cloudinary.com/dc0aja7yx/image/upload/v1731014115/9_cnvglh.png",  // Cambia a la ruta real de la imagen
       color: "#60a5fa",
+      description: "",
+      modules: ["","",""],
+      category: "Gestión de la calidad",
+      price: 30000
     },
   ];
 
