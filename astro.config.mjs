4522b83e326f7662ee14c849e516ebdb1b9fe4c7
AlejoYela <andres.alejo.yela@gmail.com>
2024-12-04 12:45:53 -0500

    Absolute paths added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import path from 'path';
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -7,5 +8,14 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'), 
+      '@components': path.resolve('./src/components'), // Alias para 'components'
+      '@icons': path.resolve('./src/icons'), // Alias para 'icons' // Define el alias '@' para apuntar a la carpeta 'src'
+      '@layouts': path.resolve('./src/layouts'), // Alias para 'layouts' // Define el alias '@' para apuntar a la carpeta 'src'
+      '@pages': path.resolve('./src/pages'), // Alias para 'pages' // Define el alias '@' para apuntar a la carpeta 'src'
+    },
+  },
 });
